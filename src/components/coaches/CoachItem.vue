b03de2266c26bccdc28c97c892de75e70cd14984
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-09-14 09:09:23 -0300

    Added CoachItem

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <h3>{{fullName}}</h3>
+    <h4>${{rate}}/hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{area}}</span>
+    </div>
+    <div class="actions">
+      <router-link to="/coaches/c1/contact">Contact</router-link>
+      <router-link to="/coaches/c1">View Details</router-link>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
