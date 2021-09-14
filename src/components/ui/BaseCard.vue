d8e1bc024e432cc6dcf45aefe5119fc547f2589c
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-09-14 09:49:52 -0300

    Added BaseCard to the project

--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -13,7 +13,7 @@ export default {
 <style scoped>
 .card {
   border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  box-shadow: 0 2px 8px rgba(180, 76, 147, 0.26);
   padding: 1rem;
   margin: 2rem auto;
   max-width: 40rem;
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,25 +4,29 @@
       FILTER
     </section>
     <section>
-      <div class="constrols">
-        <button>Refresh</button>
-        <router-link to="/register">Register as Coach</router-link>
-      </div>
-      <!-- <ul  v-if="hasCoaches"> -->
-      <ul>
-        <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-          :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
-      </ul>
-      <!-- <h3 v-else>No coaches found.</h3> -->
+      <base-card>
+        <div class="constrols">
+          <button>Refresh</button>
+          <router-link to="/register">Register as Coach</router-link>
+        </div>
+        <!-- <ul  v-if="hasCoaches"> -->
+        <ul>
+          <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+            :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+        </ul>
+        <!-- <h3 v-else>No coaches found.</h3> -->
+      </base-card>
     </section>
   </div>
 </template>
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue'
+import BaseCard from '../../components/ui/BaseCard.vue'
 export default {
   components: {
-    CoachItem
+    CoachItem,
+    BaseCard
   },
   computed: {
     filteredCoaches(){
