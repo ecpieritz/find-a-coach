7ca5cd8429bb5f77bce1075cd5223b6aad242538
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-09-14 10:02:00 -0300

    Added style to BaseButton

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -29,6 +29,45 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+button,
+a {
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font: inherit;
+  background-color: #70c3e9;
+  border: 1px solid #70c3e9;
+  color: white;
+  cursor: pointer;
+  border-radius: 30px;
+  margin-right: 0.5rem;
+  display: inline-block;
+}
 
+a:hover,
+a:active,
+button:hover,
+button:active {
+  background-color: #3886aa;
+  border-color: #3886aa;
+}
+
+.flat {
+  background-color: transparent;
+  color: #70c3e9;
+  border: none;
+}
+
+.outline {
+  background-color: transparent;
+  border-color: #3886aa;
+  color: #3886aa;
+}
+
+.flat:hover,
+.flat:active,
+.outline:hover,
+.outline:active {
+  background-color: #a9d7ec;
+}
 </style>
