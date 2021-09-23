bc6ccc489e4902dc4337a046ca5dfbe00af54ddd
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-09-23 10:21:29 -0300

    Added endings to pages

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,45 +1,106 @@
 <template>
   <div>
+    <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
     <section>
-      FILTER
+      <coach-filter @change-filter="setFilters"></coach-filter>
     </section>
     <section>
       <base-card>
-        <div class="constrols">
-          <base-button mode="outline">Refresh</base-button>
-          <base-button link to="/register">Register as Coach</base-button>
+        <div class="controls">
+          <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
+          <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
         </div>
-        <!-- <ul  v-if="hasCoaches"> -->
-        <ul>
-          <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
-            :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
         </ul>
-        <!-- <h3 v-else>No coaches found.</h3> -->
+        <h3 v-else>No coaches found.</h3>
       </base-card>
     </section>
   </div>
 </template>
 
 <script>
-import CoachItem from '../../components/coaches/CoachItem.vue'
-import BaseCard from '../../components/ui/BaseCard.vue'
+import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
+
 export default {
   components: {
     CoachItem,
-    BaseCard
+    CoachFilter,
+  },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
   },
   computed: {
-    filteredCoaches(){
-      return this.$store.getters['coaches/coaches']
+    isCoach() {
+      return this.$store.getters['coaches/isCoach'];
     },
-    hasCoaches(){
-      return this.$store.getters['coaches/hasCoaches']
-    }
-  }
-}
+    filteredCoaches() {
+      const coaches = this.$store.getters['coaches/coaches'];
+      return coaches.filter((coach) => {
+        if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if (this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if (this.activeFilters.career && coach.areas.includes('career')) {
+          return true;
+        }
+        return false;
+      });
+    },
+    hasCoaches() {
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+  created() {
+    this.loadCoaches();
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters;
+    },
+    async loadCoaches(refresh = false) {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('coaches/loadCoaches', {
+          forceRefresh: refresh,
+        });
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
 ul {
   list-style: none;
   margin: 0;
